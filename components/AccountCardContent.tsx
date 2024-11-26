ba7d1ba6d14a38635075647817e4e75406d89cd9
arijit <arijitchhatui.me@gmail.com>
2024-11-26 22:40:43 +0530

    feat : message delete functionality

--- components/AccountCardContent.tsx
+++ components/AccountCardContent.tsx
@@ -2,7 +2,14 @@
 
 import useTicketAPI from "@/hooks/api/useTicketAPI";
 import { TicketsEntity } from "@/hooks/entities/tickets.entities";
-import { Avatar, Card, CardContent, CardHeader } from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+} from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
 import moment from "moment";
@@ -11,7 +18,7 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function AccountCardContentPage() {
-  const { getTickets } = useTicketAPI();
+  const { getTickets, deleteTicket } = useTicketAPI();
   const router = useRouter();
   const [tickets, setTickets] = useState<TicketsEntity[]>([]);
   const loadTickets = async () => {
@@ -23,24 +30,38 @@ export default function AccountCardContentPage() {
       toast.error("Failed to load tickets");
     }
   };
+
+  const handleDelete = async (ticketId: string) => {
+    try {
+      await deleteTicket(ticketId);
+      toast.success("Deleted");
+      setTickets((pre) => pre.filter((tkt) => tkt._id !== ticketId));
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete ticket!");
+    }
+  };
+
   useEffect(() => {
     loadTickets();
   }, []); //eslint-disable-line
 
   return (
     <Grid container spacing={2} columns={12} mb={10}>
-      {tickets.map((ticket, ) => (
+      {tickets.map((ticket) => (
         <Grid key={ticket._id} size={{ xs: 12, md: 6 }}>
-          <Card
-            variant="outlined"
-            onClick={() => router.push(`/ticket/${ticket._id}`)}
-          >
+          <Card variant="outlined">
             <CardHeader
               avatar={<Avatar src="/icons/rail.png" alt="User" />}
               title={ticket.sourceStation}
               subheader={moment(ticket.bookingTime).format("DD/MM/YYYY")}
+              action={
+                <IconButton onClick={() => handleDelete(ticket._id)}>
+                  <DeleteOutlineIcon />
+                </IconButton>
+              }
             />
-            <CardContent>
+            <CardContent onClick={() => router.push(`/ticket/${ticket._id}`)}>
               <>
                 <Typography gutterBottom variant="caption" component="div">
                   {ticket.destinationStation}
